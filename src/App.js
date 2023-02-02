103126ee90da2c31e9466904446e3879c001cd3a
Yasmim Ramos <yasmimrlopes2018@gmail.com>
2023-02-02 17:57:20 -0300

    Adicionei e mudei texto no app para primeiro teste

--- src/App.js
+++ src/App.js
@@ -3,20 +3,15 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    // JSX, não HTML
+    // para atribuir classe ao elemento, usa-se className
+    <div className="App"> 
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <b>UN-TAKE ME</b> - React
+          
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
