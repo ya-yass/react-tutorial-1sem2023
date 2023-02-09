3cd58d7636884055472c0d99dadf9f9287bac8a3
Yasmim Ramos <yasmimrlopes2018@gmail.com>
2023-02-09 17:58:57 -0300

    aaa

--- src/index.js
+++ src/index.js
@@ -1,17 +1,19 @@
+// Arquivo que contém o componente "mãe" da aplicação, nele é importado os demais componentes em forma de tag
+// Os componentes são criados separadamente em arquivos .js .html .css 
+// Os componentes não usa camelCase, sempre irão começar com Maiúscula
+
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import './index.css'; // para importar CSS basta colocar import 'arquivo.css' | não precisa import from 'arquivo.css'
+                      // css importado no index vale para toda a aplicação
+import App from './App'; // para importar .js não é necessário usar a extensão do arquivo para que o programa o reconheça
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+  // requisita 'root' para que tudo o que vem depois apareça na página
 root.render(
   <React.StrictMode>
+    {/* carrega o componente duas vezes para que quando a ação for abortada, ela pare a requisição | um exemplo é quando o usuário requisitou uma informação dentro do banco de dados, mas logo depois cancelou o pedido; StrictMode serve para comunicar o banco que a ação foi abortada */}
     <App />
+    {/* <div>Olá Mundo</div> */}
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
