79f6bf1b0bfa123b785a5847f15119f0b8ba2c59
Yasmim Ramos <yasmimrlopes2018@gmail.com>
2023-02-16 18:01:26 -0300

    PJ3 16/02/2023 - Criando componentes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+/*
+import logo from './logo.svg'; //arquivo de imagem importa com FROM
+import './App.css'; //arquivo css importa direto
+import Header from './components/Header'
 
-function App() {
-  return (
-    // JSX, não HTML
+//dentro de return, a primeira seção não pode ter irmão, somente uma seção pai
+
+/*function App() { //modo antigo de programar
+  return ( //tudo o que estiver em return vai retornar (como resposta) no index, onde a função foi chamada
+    // é JSX, não HTML, uma linguagem semelhante
     // para atribuir classe ao elemento, usa-se className
     <div className="App"> 
       <header className="App-header">
@@ -15,6 +19,29 @@ function App() {
       </header>
     </div>
   );
+}//
+
+const App = () =>{ // modo atual de programar
+  return(
+    <> 
+    <h1>aaa</h1>
+    <p> "<></>" é um <b>fragment</b>, usado como seção pai para que todo o restante do código prossiga normalmente</p>
+    </>
+  )
+}
+export default App; //tag necessária para exportar o componente
+*/
+
+import Header from './components/Header'
+import MainMenu from './components/MainMenu'
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <MainMenu />
+    </>
+  )
 }
 
-export default App;
+export default App
