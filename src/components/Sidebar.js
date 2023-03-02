eddb32c214023430c391a5e43f27217c2529fd5d
Yasmim Ramos <yasmimrlopes2018@gmail.com>
2023-03-02 17:54:12 -0300

    adicionando biblioteca de icon e svg

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,4 +1,10 @@
 import Box from '@mui/material/Box';
+import fotoTwt from './assets/img/twt.jpg';
+import { ReactComponent as IconFoice } from './assets/img/icon-foice.svg';
+import {
+  FaCartPlus as iconCarrinho, 
+  FaAccessibleIcon as IconRun
+} from 'react-icons/fa'
 
 const Sidebar = () => {
   return (
@@ -8,6 +14,30 @@ const Sidebar = () => {
       height: 500,
     }}>
       <h2>Sidebar</h2>
+      <img
+        style={{
+          width: 200,
+        }}
+        src="https://t.ctcdn.com.br/essK16aBUDd_65hp5umT3aMn_i8=/400x400/smart/filters:format(webp)/i606944.png" />
+
+      <img src={fotoTwt} />
+
+      <hr></hr>
+
+      <IconFoice 
+        style={{
+        width: 30,
+        height: 30
+      }} />
+
+      <hr></hr>
+
+      <IconRun />
+
+      <hr></hr>
+
+      <iconCarrinho />
+
     </Box>
   )
 }
